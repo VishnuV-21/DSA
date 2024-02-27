03447319d203a9fd00cccc00ee8f628f443430d5
VishnuV-21 <vishnuvs5171@gmail.com>
2024-02-27 09:17:02 +0530

    Added info on time and space complexity for Linear search .

--- DSA basics/src/com/search_algo/LinearSearch.java
+++ DSA basics/src/com/search_algo/LinearSearch.java
@@ -2,6 +2,22 @@ package com.search_algo;
 
 import java.util.Scanner;
 
+
+/*Time Complexity Explanation:
+
+In the worst case, you might need to go through each element in the list until you find the target element or reach the end.
+The time it takes to complete the search is directly proportional to the number of elements in the list (n).
+As you increase the size of the input, the time taken by linear search grows linearly.
+
+Space Complexity Explanation:
+
+Linear search does not use any additional data structures that depend on the size of the input.
+It typically requires only a constant amount of extra space for variables like loop counters and temporary storage.
+The space complexity remains constant, regardless of the size of the input list.
+While linear search is straightforward, it is not the most efficient for large data sets compared to other algorithms like
+binary search on a sorted list. However, its simplicity and low space complexity make it suitable for small lists or 
+unordered data sets.*/
+
 public class LinearSearch {
 	static int arr[]= {1,3,5,7,9,2,4,6,8};
 
